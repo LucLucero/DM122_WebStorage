b8089cc172729de5a03fc61ec6a066c7ab55a6d8
LucLucero <lucianoxlucero@hotmail.com>
2022-10-28 00:08:30 -0300

    feat: Save to LocalStorage

--- localStorage/app.js
+++ localStorage/app.js
@@ -11,8 +11,31 @@ new App();
 const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
 
-    event.preventDefault();
-    console.log('Submitted Successfully');
+    event.preventDefault();    
+    console.log({event});
+    const form = event.target;
+    const formData = {
 
+        key: form.key.value,
+        value:form.value.value,
+
+    };
+
+    console.log({formData});
+    window.localStorage.setItem('key', form.key.value);
+    window.localStorage.setItem('value', form.value.value);
+    readFromStorage()
+    
 
 });
+
+
+function readFromStorage() {
+
+    const key = window.localStorage.getItem('key');
+    const value = window.localStorage.getItem('value');
+    document.querySelector('output')
+        .textContent = JSON.stringify( {key, value}, null,2);
+}
+
+readFromStorage()
