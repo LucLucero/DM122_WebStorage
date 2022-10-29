9023053fe6dfceb39e532ce6ecd13f36c353803c
LucLucero <lucianoxlucero@hotmail.com>
2022-10-29 17:05:14 -0300

    Final commit- Some examples LocalStorage CRUD

--- localStorage/app.js
+++ localStorage/app.js
@@ -14,28 +14,56 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();    
     console.log({event});
     const form = event.target;
+    console.log({form});
     const formData = {
 
         key: form.key.value,
-        value:form.value.value,
+        value: form.value.value,
 
     };
 
     console.log({formData});
-    window.localStorage.setItem('key', form.key.value);
-    window.localStorage.setItem('value', form.value.value);
-    readFromStorage()
+
+
+
+        window.localStorage.setItem(form.key.value, form.value.value);
+        form.reset();
+        form.key.focus();
+        readFromStorage();
     
+
 
 });
 
 
 function readFromStorage() {
+      
 
-    const key = window.localStorage.getItem('key');
-    const value = window.localStorage.getItem('value');
-    document.querySelector('output')
-        .textContent = JSON.stringify( {key, value}, null,2);
+    const key = window.localStorage.getItem(form.key.value);
+    const value= window.localStorage.getItem(form.value.value);
+    document.querySelector('output').innerHTML = Object.keys(window.localStorage).map(htmlTemplate).join("")
 }
 
-readFromStorage()
+function htmlTemplate(key){
+
+  const value = window.localStorage.getItem(key);
+  return `
+    <span>${key}</span>
+    <span>${value}</span>
+    <span><img src=./images/lixo.svg width="12" onclick="removeItem('${key}')"></span>
+    `
+}
+
+function removeItem(key){
+
+  if(confirm("Are you sure you want to remove this item permanently???")){
+    window.localStorage.removeItem(key);
+    readFromStorage();
+  }
+
+
+}
+
+
+
+readFromStorage();
