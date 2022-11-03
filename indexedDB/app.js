781637b6921205d8e74494b38d055e9721e0f401
LucLucero <lucianoxlucero@hotmail.com>
2022-11-03 00:00:45 -0300

    feat: How to use JSModules

---
+++ indexedDB/app.js
@@ -0,0 +1,7 @@
+console.log("arquivo certo");
+//import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
+import { MyFirstModule, MySecondModule } from "./myFirstModule.js";
+
+
+MyFirstModule("Hello!");
+
---
+++ indexedDB/myFirstModule.js
@@ -0,0 +1,9 @@
+export function MyFirstModule(param){
+
+    console.log(param);
+}
+export function MySecondModule(param){
+
+    console.log(param);
+
+}
