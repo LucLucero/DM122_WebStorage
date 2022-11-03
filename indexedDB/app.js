d19625cd97a5fc2153c028acd4183bddfd6fd079
LucLucero <lucianoxlucero@hotmail.com>
2022-11-03 01:17:57 -0300

    feat: Add Using Dexie for indexedDB

--- indexedDB/app.js
+++ indexedDB/app.js
@@ -1,7 +1,41 @@
-console.log("arquivo certo");
-//import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
-import { MyFirstModule, MySecondModule } from "./myFirstModule.js";
+import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
+
+const db = new Dexie('pokemonDB');
+
+//table = store
+
+db.version(1).stores({
+
+    pokemon: "++id, name",
 
 
-MyFirstModule("Hello!");
+});
 
+
+db.on('populate', async () => {
+
+    await db.pokemon.bulkPut([
+    
+        {name: "Venusaur",
+        profile: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+        },
+    
+        {name: "Gengar",
+        profile: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png"
+        },
+    
+        {name: "Magmar",
+        profile: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/126.png"
+        },
+    
+    ]);
+    
+})
+db.open();
+
+
+//const queryPokemon = await db.pokemon
+//    .where('name').equals('gengar')
+//   .toArray();
+
+    
