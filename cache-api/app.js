b3f7ed7ca08fcf91334352289ada40692d5d415f
LucLucero <lucianoxlucero@hotmail.com>
2022-11-07 22:42:32 -0300

    feat: implement fetch pokemon data

--- cache-api/app.js
+++ cache-api/app.js
@@ -8,9 +8,13 @@ async function fetchPokeData (){
 
 async function showPokeData() {
 
+    const img = document.querySelector('img');
+    const details = document.querySelector('details');
     const pre = document.querySelector('pre');
-    const pokeData = await fetchPokeData();
-    pre.textContent = JSON.stringify(pokeData,null,2);
-    
-
+    const pokeData = await fetchPokeData();    
+    pre.textContent = JSON.stringify(pokeData,null,2);   
+    img.src = pokeData.sprites.other['official-artwork'].front_default;
+    img.alt = pokeData.name;    
+    img.title = pokeData.name;
+    details.hidden = false;
 }
