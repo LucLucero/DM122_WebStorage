32808b4c8143834d2f31e1fb86422ffd22d2f7bd
LucLucero <lucianoxlucero@hotmail.com>
2022-11-07 23:38:18 -0300

    feat: Add fetchFromNetWork and FetchFromCache

--- cache-api/app.js
+++ cache-api/app.js
@@ -1,17 +1,40 @@
-async function fetchPokeData (){
+const CACHE_KEY = 'poke-cache-v1'; // nome do cache
 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/282')
+async function addToCache(requestURL, response){
+
+    const cache = await caches.open(CACHE_KEY); //abrindo o cache e passando o cache key
+    cache.put(requestURL, response);
+
+}
+
+async function fetchFromNetwork (requestURL){
+
+    const response = await fetch(requestURL)
+    addToCache(requestURL, response.clone());
     const json = await response.json();
     return json;
 
 }
+
+
+async function fetchFromCache(requestURL){
+
+    const cache = await caches.open(CACHE_KEY);
+    const cachedResponse = await cache.match(requestURL);
+    const pokeData = cachedResponse?.json();
+    return pokeData;
+    
+}
+
 
 async function showPokeData() {
 
     const img = document.querySelector('img');
     const details = document.querySelector('details');
     const pre = document.querySelector('pre');
-    const pokeData = await fetchPokeData();    
+    const requestURL = "https://pokeapi.co/api/v2/pokemon/282"
+    const pokeData = 
+        (await fetchFromCache(requestURL)) || (await fetchFromNetwork(requestURL));
     pre.textContent = JSON.stringify(pokeData,null,2);   
     img.src = pokeData.sprites.other['official-artwork'].front_default;
     img.alt = pokeData.name;    
